23d6239604c888d628bf65f353b35823a4135add
gimmenitram <martin.bangayan@gmail.com>
2021-12-06 21:56:29 +1300

    send email notification

---
+++ sendmail.py
@@ -0,0 +1,18 @@
+import smtplib, ssl
+
+port = 465 # SSL/TLS port
+smtp_server = "smtp.gmail.com"
+sender_email = ""
+password = ""
+receiver_email = ""
+message = """\
+To: {}
+Subject: TEST - Python
+
+Test message from Python app.
+""".format(receiver_email)
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
